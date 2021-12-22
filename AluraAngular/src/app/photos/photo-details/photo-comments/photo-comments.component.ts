e73103c8b1b5589060eaf7f4bfb64dd095aad8db
EliasMurat <elias-murat@outlook.com>
2021-12-22 11:13:03 -0300

    add refresh com rxjs

--- AluraAngular/src/app/photos/photo-details/photo-comments/photo-comments.component.ts
+++ AluraAngular/src/app/photos/photo-details/photo-comments/photo-comments.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
+import { switchMap, tap } from 'rxjs/operators';
+
 import { PhotoComment } from '../../photo/photo-comment';
 import { PhotoService } from '../../photo/photo.service';
 
@@ -22,7 +24,7 @@ export class PhotoCommentsComponent implements OnInit {
     private photoService: PhotoService,
     private formBuilder: FormBuilder
   ) {
-    this.commentForm = formBuilder.group({
+    this.commentForm = this.formBuilder.group({
       comment: ['', Validators.maxLength(255)]
     });
   }
@@ -36,15 +38,14 @@ export class PhotoCommentsComponent implements OnInit {
   }
 
   save() {
-    let comment: string = this.commentForm.get('comment')?.value;
-    this.photoService.addComment(this.photoId, comment)
-      .subscribe(
-        () => {
-          this.commentForm.reset();
-          alert('Comentário adicionado com sucesso!');
-          this.getCommentsByPhotoId();
-        }
-      );
+    const comment = this.commentForm.get('comment')?.value as string;
+    this.comments$ = this.photoService
+      .addComment(this.photoId, comment)
+      .pipe(switchMap(() => this.photoService.getComments(this.photoId)))
+      .pipe(tap(() => {
+        this.commentForm.reset();
+        alert('Comentário adicionado com sucesso');
+      }
+      ));
   }
-
 }
