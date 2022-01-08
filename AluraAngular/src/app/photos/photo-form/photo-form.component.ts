950bbae87b01c2564da301a9985576070da922d1
EliasMurat <elias-murat@outlook.com>
2022-01-08 11:43:31 -0300

    finalizando upload e progresso

--- AluraAngular/src/app/photos/photo-form/photo-form.component.ts
+++ AluraAngular/src/app/photos/photo-form/photo-form.component.ts
@@ -1,6 +1,8 @@
+import { HttpEvent, HttpEventType } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { finalize } from 'rxjs/operators';
 import { UserService } from 'src/app/core/user/user.service';
 import { AlertService } from 'src/app/shared/components/alert/alert.service';
 
@@ -16,6 +18,7 @@ export class PhotoFormComponent implements OnInit {
   photoForm: FormGroup;
   file: File | null = null;
   preview: string = '';
+  percentDone: number = 0;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -48,10 +51,24 @@ export class PhotoFormComponent implements OnInit {
 
     if (this.file !== null) {
       this.photoService.uploadPhoto(description, allowComments, this.file)
-        .subscribe(() => {
-          this.alertService.success('Upload complete', true);
+        .pipe(finalize(() => {
           this.router.navigate(['/user', this.userService.getUserName()]);
-        });
+        }))
+        .subscribe(
+          (event: HttpEvent<any>) => {
+            if (event.type === HttpEventType.UploadProgress) {
+              if (event.total) {
+                this.percentDone = Math.round(100 * event.loaded / event.total);
+              }
+            } else if (event.type === HttpEventType.Response) {
+              this.alertService.success('Upload complete', true);
+            }
+          },
+          (err) => {
+            console.log(err);
+            this.alertService.danger('Upload error!', true);
+          }
+        );
     }
   }
 
--- AluraAngular/src/app/photos/photo/photo.service.ts
+++ AluraAngular/src/app/photos/photo/photo.service.ts
@@ -30,7 +30,14 @@ export class PhotoService {
     formData.append('allowComments', allowComments ? 'true' : 'false');
     formData.append('imageFile', file);
 
-    return this.http.post(API + 'photos/upload', formData);
+    return this.http.post(
+      API + 'photos/upload',
+      formData,
+      {
+        observe: 'events',
+        reportProgress: true
+      }
+    );
   }
 
   findById(photoId: number) {
