705827af035f37f9f25a5cd755b6a7df6a739ce5
EliasMurat <elias-murat@outlook.com>
2022-01-15 11:48:53 -0300

    criando barra de loading

--- AluraAngular/src/app/core/core.module.ts
+++ AluraAngular/src/app/core/core.module.ts
@@ -1,3 +1,4 @@
+import { LoadingModule } from './../shared/components/loading/loading.module';
 import { CommonModule } from '@angular/common';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
@@ -16,7 +17,8 @@ import { HeaderComponent } from './header/header.component';
   imports: [
     CommonModule,
     RouterModule,
-    AlertModule
+    AlertModule,
+    LoadingModule
   ],
   exports: [
     HeaderComponent,
---
+++ AluraAngular/src/app/shared/components/loading/loading.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { LoadingService } from './loading.service';
+
+@Component({
+  selector: 'app-loading',
+  templateUrl: './loading.component.html',
+  styleUrls: ['./loading.component.css']
+})
+export class LoadingComponent implements OnInit {
+
+  loading$: Observable<string>;
+
+  constructor(private loadingService: LoadingService) {
+    this.loading$ = this.loadingService.getLoading()
+      .pipe(map(loadingType => loadingType.valueOf()));
+  }
+
+  ngOnInit() { }
+
+}
